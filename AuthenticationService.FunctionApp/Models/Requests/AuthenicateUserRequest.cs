20e50a56c86737a35e9ac112067b89acbd0e9056
juanE <55924277+je-vargas@users.noreply.github.com>
2021-07-09 10:50:38 +0100

    Request Models added

---
+++ AuthenticationService.FunctionApp/Models/Requests/AuthenicateUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationService.FunctionApp.Models.Requests
+{
+    public class AuthenicateUserRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
